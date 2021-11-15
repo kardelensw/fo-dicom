70b4b5d2b9c9fc9764810c5f4dcdebafd7911137
Ahmet Altay <46381367+ahmetaltay33@users.noreply.github.com>
2021-11-15 09:18:27 +0300

    project versions and urls are modified to be kardelensw

---
+++ PushToBaget.ps1
@@ -0,0 +1,7 @@
+Remove-Item .\.output\ -Recurse
+dotnet clean .\FO-DICOM.Full.sln -o .\.output
+dotnet build .\FO-DICOM.Full.sln -o .\.output
+dotnet pack .\FO-DICOM.Full.sln -o .\.output\packages
+dotnet nuget push -s http://nuget.kardelenyazilim.com:5555/v3/index.json .\.output\packages\fo-dicom.NLog.5.0.0-krmd.nupkg
+dotnet nuget push -s http://nuget.kardelenyazilim.com:5555/v3/index.json .\.output\packages\fo-dicom.Imaging.ImageSharp.5.0.0-krmd.nupkg
+dotnet nuget push -s http://nuget.kardelenyazilim.com:5555/v3/index.json .\.output\packages\fo-dicom.5.0.0-krmd.nupkg
