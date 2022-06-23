de8f08baab98ac93f05607fa061d78659226b5f1
Ahmet Altay <46381367+ahmetaltay33@users.noreply.github.com>
2022-06-23 17:52:59 +0300

    Upgraded 3 party packages and update package versions.

--- PushToBaget.ps1
+++ PushToBaget.ps1
@@ -2,6 +2,6 @@ Remove-Item .\.output\ -Recurse
 dotnet clean .\FO-DICOM.Full.sln -o .\.output
 dotnet build .\FO-DICOM.Full.sln -o .\.output
 dotnet pack .\FO-DICOM.Full.sln -o .\.output\packages
-dotnet nuget push -s http://nuget.kardelenyazilim.com:5555/v3/index.json .\.output\packages\fo-dicom.NLog.5.0.1-krmd.nupkg
-dotnet nuget push -s http://nuget.kardelenyazilim.com:5555/v3/index.json .\.output\packages\fo-dicom.Imaging.ImageSharp.5.0.1-krmd.nupkg
-dotnet nuget push -s http://nuget.kardelenyazilim.com:5555/v3/index.json .\.output\packages\fo-dicom.5.0.1-krmd.nupkg
+dotnet nuget push -s http://nuget.kardelenyazilim.com:5555/v3/index.json .\.output\packages\fo-dicom.NLog.5.0.3-krmd.nupkg
+dotnet nuget push -s http://nuget.kardelenyazilim.com:5555/v3/index.json .\.output\packages\fo-dicom.Imaging.ImageSharp.5.0.3-krmd.nupkg
+dotnet nuget push -s http://nuget.kardelenyazilim.com:5555/v3/index.json .\.output\packages\fo-dicom.5.0.3-krmd.nupkg
